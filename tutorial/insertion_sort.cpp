db06568a8eb2a856813d89a54050ee89ce13e01e
Hawkwatch19 <nadim.sarker461@gmail.com>
2024-03-18 04:59:22 +0600

    insertion sort done

--- tutorial/insertion_sort.cpp
+++ tutorial/insertion_sort.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
+    int arr[] = {12,45,23,53,19,8};
+    for (int i = 1; i < 6; i++)
+    {
+        int current = arr[i];
+        int j = i-1;
+        while(arr[j]>current && j >=0)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
     
 }
