a86b9c03efd63b53fe06fa89ee3c75b0a2e72c99
Hawkwatch19 <nadim.sarker461@gmail.com>
2024-04-03 02:55:45 +0600

    solution of 1900A

--- Codeforce/1900A_Cover_in_water.cpp
+++ Codeforce/1900A_Cover_in_water.cpp
@@ -1,12 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int t; cin>>t;
+    int t;
+    cin >> t;
     while (t--)
     {
-        
+        int x;
+        cin >> x;
+        string str;
+        cin >> str;
+        int c = 0;
+        for (int i = 0; i < x; i++)
+        {
+            if (str[i] == '.')
+            {
+                c++;
+            }
+            if (str[i] == '.' && str[i + 1] == '.' && str[i + 2] == '.')
+            {
+                c = 2;
+                break;
+            }
+        }
+        cout << c << endl;
     }
-    
 }
