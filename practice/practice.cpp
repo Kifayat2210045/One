b89c39fbc9742013df1ee6e5b3471a6171655b93
Hawkwatch19 <nadim.sarker461@gmail.com>
2024-04-03 02:56:54 +0600

    contest problem solve

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -3,18 +3,5 @@ using namespace std;
 
 int main()
 {
-    int a, b, k;
-    cin >> a >> b >> k;
-    int c = 0;
-    for (int i = a; i <= b; i++)
-    {
-        for (int j = a; j <= b; j++)
-        {
-            if (i * j <= k)
-            {
-                c++;
-            }
-        }
-    }
-    cout << c << endl;
+    cout<<"Muntasir Fahim"<<endl;
 }
