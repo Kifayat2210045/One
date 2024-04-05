a77974df7881911289d2a60752201f9acf4c6c23
Hawkwatch19 <nadim.sarker461@gmail.com>
2024-04-05 15:55:05 +0600

    solving 1890A

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -9,40 +9,26 @@ int main()
     {
         int n;
         cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++)
+        map<int, int> occ;
+        for (int i = 1; i <= n; ++i)
         {
-            cin >> arr[i];
+            int x;
+            cin >> x;
+            occ[x]++;
         }
-        int index = 0;
-        int arr2[n];
-        for (int i = 0; i < n; i++)
-        {
-            bool k = true;
-            for (int j = 0; j <= index; j++)
-            {
-                if (arr[i] == arr2[j])
-                {
-                    k = false;
-                    break;
-                }
-            }
-            if(k)
-            {
-                arr2[index] = arr[i];
-                index++;
-            }
-        }
-        if(n == 2)
-        {
-            cout<<"YES"<<endl;
-        }
-        if(index>2)
-        {
+        if (occ.size() > 2)
             cout<<"NO"<<endl;
-        }
+        else
         {
-
+            if (abs(occ.begin()->second - occ.rbegin()->second) <= 1)
+            {
+                cout<<"YES"<<endl;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+            }
         }
     }
+    return 0;
 }
