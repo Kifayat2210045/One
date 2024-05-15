2745404cbb3f7ce82a2cab1a0a3ac8109e0b02dc
Nadeem Hossain <nadim.sarker461@gmail.com>
2024-05-15 15:10:08 +0600

    nsi

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-    cout<<"hello world"<<endl;
+int main() {
+    vector<int> dir = {1,2,3,4,5};
+    for (auto d : dir)
+    {
+        cout<<d<<endl;
+    }
+    return 0;
 }
