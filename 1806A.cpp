2a0ba2be4644046b1d0162d567cfe7da72941580
Nadeem Hossain <nadim.sarker461@gmail.com>
2024-07-10 19:04:17 +0600

    solution of 1806A

---
+++ 1806A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define ll long long int
+#define ull unsigned long long
+#define vll vector<ll>
+#define vint vector<int>
+#define debug(x) cout << x << nl
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, c, d;
+        cin >> a >> b >> c >> d;
+        ll add = d - b;
+        a += add;
+        b += add;
+        if (a < c || add < 0)
+            cout << -1 << nl;
+        else
+        {
+            add += a - c;
+            cout << abs(add) << nl;  
+        }
+            
+    }
+}
