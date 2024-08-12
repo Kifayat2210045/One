1c488e6b0d526f2be5b600345e4551255eb681c7
Nadeem Hossain <nadim.sarker461@gmail.com>
2024-08-12 16:04:46 +0600

    solution of 1761A

---
+++ 1761A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define yes cout<< "YES" <<nl
+#define no cout<< "NO" <<nl
+#define ll long long int
+#define ull unsigned long long
+#define vll vector<ll>
+#define vint vector<int>
+#define debug(x) cout<<x<<nl
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,a,b;
+        cin>>n>>a>>b;
+        if(a==b && b == n) yes;
+        else if (n-(a+b) <= 1)
+        {
+            no;
+        }
+        else yes;
+        
+    }
+}
