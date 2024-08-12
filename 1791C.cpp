f16e0ecb50feb964c918d56ccb6a068454cc6667
Nadeem Hossain <nadim.sarker461@gmail.com>
2024-08-12 16:06:56 +0600

    solution of 1791C

---
+++ 1791C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define ll long long int
+#define ull unsigned long long
+#define vll vector<ll>
+#define vint vector<int>
+#define debug(x) cout << x << nl
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin>>s;
+        int i = 0, j = n - 1;
+        while (n > 0 && s[i] != s[j])
+        {
+            n -= 2;
+            i++;
+            j--;
+        }
+        cout << n << nl;
+    }
+}
