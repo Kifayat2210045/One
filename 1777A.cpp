c7eec4d9f1d4c08f195e66c596323b14d5fa3569
Nadeem Hossain <nadim.sarker461@gmail.com>
2024-08-12 16:05:50 +0600

    solution of 1777A

---
+++ 1777A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define ll long long int
+#define ull unsigned long long
+#define vll vector<ll>
+#define vint vector<int>
+#define debug(x) cout << x << nl
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vint v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        int c = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if ((v[i] % 2 != 0 && v[i + 1] % 2) || (v[i] % 2 == 0 && v[i + 1] % 2 == 0))
+            {
+                c++;
+            }
+        }
+        cout<<c<<nl;
+    }
+}
