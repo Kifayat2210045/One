eb4ec6b2e59f728e12d5b8f478a66c6820732706
Nadeem Hossain <nadim.sarker461@gmail.com>
2024-08-12 16:10:54 +0600

    solution of 1999A

---
+++ 1999A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define ll long long int
+#define ull unsigned long long
+#define vll vector<ll>
+#define vint vector<int>
+#define debug(x) cout << x << nl
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int sum = 0;
+        sum += n % 10;
+        n = n / 10;
+        sum += n;
+        cout << sum << nl;
+    }
+}
